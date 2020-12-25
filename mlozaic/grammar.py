867ad6795465f8cb1c8a6443fd464bb4a5fb6eeb
Kavi Gupta <kavi@berkeley.edu>
2020-12-25 14:29:33 -0800

    add calculation for alphabet

--- mlozaic/grammar.py
+++ mlozaic/grammar.py
@@ -33,3 +33,20 @@ grammar = {
         [IfE, "C", "D", "D"],
     ],
 }
+
+
+def alphabet(g):
+    if isinstance(g, dict):
+        for v in g.values():
+            yield from alphabet(v)
+    elif isinstance(g, list):
+        for v in g:
+            yield from alphabet(v)
+    elif isinstance(g, str):
+        return
+    else:
+        yield from g.tags()
+
+
+ALPHABET = sorted(set(alphabet(grammar)) | set("()"))
+BACKWARDS_ALPHABET = {sym: i for i, sym in enumerate(ALPHABET)}
